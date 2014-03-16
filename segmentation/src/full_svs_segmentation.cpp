83239fab6a3219332ee3b61b307fc58e7c45150e
Luke Fraser <Luke.Fraser.A@gmail.com>
2014-03-16 00:23:44 -0700

    reading images and exporting layers as well as dimensions of each layer

--- segmentation/src/full_svs_segmentation.cpp
+++ segmentation/src/full_svs_segmentation.cpp
@@ -1,7 +1,24 @@
 #include "stdlib.h"
+#include <stdio.h>
 #include "openslide/openslide.h"
 
 int main( int argc, char* argv[] ){
 
+	const char * vendor = openslide_detect_vendor(argv[1]);
+	printf("%s\n", vendor );
+
+	openslide_t * image = openslide_open(argv[1]);
+	int levels = openslide_get_level_count(image);
+	printf("Levels: %d\n", levels);
+
+	for(int i = 0; i < levels; i++){
+		int64_t x, y;
+		openslide_get_level_dimensions(image, i, &x, &y);
+		printf("Dimensions: Width: %d , Height: %d\n", (int)x, (int)y );
+	}
+
+	// READ ACROSS ENTIRE IMAGE: THRESHOLDING FOR COLOR
+
+	
 	return 0;
-}
+}  
