beebaa956993dd465f0ec157d1e8ed8c3285db2c
Luke Fraser <Luke.Fraser.A@gmail.com>
2014-03-17 00:24:21 -0700

    adding edge detection class for svs slide fiels

---
+++ segmentation/edgedetection.h
@@ -0,0 +1,12 @@
+#ifndef TILE_EDGE_DETECTION
+#define TILE_EDGE_DETECTION
+
+#include "opencv2/opencv.hpp"
+#include "openslide/openslide.h"
+
+using namespace cv;
+
+namespace edge{
+	void slide_canny( openslide_t, )
+}
+#endif
