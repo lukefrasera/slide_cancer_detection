a0fc649cf390f201798576387e8923584300ff0b
Luke Fraser <Luke.Fraser.A@gmail.com>
2014-03-27 01:08:24 -0700

    file is no longer segfaulting and it it able to succesfully read in an entire image across the lowest level of the image. It reads the data in tiles and is going to convert the image into a tiled tiff image inorder to perfrom vision tasks on it

--- svstotiff/include/svsconvert.cpp
+++ svstotiff/include/svsconvert.cpp
@@ -1,7 +1,7 @@
 #include "svsconvert.h"
 
 
-#define TIFF_TILE_SIZE 128
+#define TIFF_TILE_SIZE 1024
 
 svsconvert::svsconvert( const char * filename){
 	image = openslide_open(filename);
@@ -34,14 +34,30 @@ TIFF * svsconvert::tiff(const char * filename){
 	// DETERMINE THE NUMBER OF TILES
 	unsigned int NUM_TILES = header.levels->w / TIFF_TILE_SIZE + (header.levels->w % 2);
 	NUM_TILES *= header.levels->h / TIFF_TILE_SIZE + (header.levels->w % 2);
+	unsigned int TILE_SIZE_WIDTH, TILE_SIZE_HEIGHT;
 
 	printf("Number of Tiles: %d\n", NUM_TILES);
 	
 
+	svsbuf = new uint32_t[TIFF_TILE_SIZE * TIFF_TILE_SIZE];
 	for( unsigned int i = 0; i < header.levels[0].w; i += TIFF_TILE_SIZE){
 		for( unsigned int j = 0; j < header.levels[0].h; j += TIFF_TILE_SIZE){
-			openslide_read_region(image, svsbuf, i, j, 0, TIFF_TILE_SIZE, TIFF_TILE_SIZE);
+			// NEED TO HANDLE THE EDGE SITUATION: MUST ONLY READ UP UNTIL THE LIMIT IN IMAGE.W && IMAGE.H
+			TILE_SIZE_WIDTH  = (header.levels[0].w - i > TIFF_TILE_SIZE) ? (TIFF_TILE_SIZE) : (header.levels[0].w - i);
+			TILE_SIZE_HEIGHT = (header.levels[0].h - j > TIFF_TILE_SIZE) ? (TIFF_TILE_SIZE) : (header.levels[0].h - j);
+
+			openslide_read_region(image, svsbuf, i, j, 0, TILE_SIZE_WIDTH, TILE_SIZE_HEIGHT);
+
+			// COPY BUFFER TO INTO TIFF TILE
+
+
 		}
 	}
+	delete [] svsbuf;
+			printf("Tile Width : %d ", TILE_SIZE_WIDTH);
+			printf("Tile Height: %d\n", TILE_SIZE_HEIGHT);
+
+	printf("Out of Four loops\n");
+
 	return NULL;
 }
